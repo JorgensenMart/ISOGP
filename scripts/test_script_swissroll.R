176e7ec2c49f8fdeac62a20f497f15210e7cb8bb
Martin <marjorg2@gmail.com>
2020-03-13 11:53:16 +0100

    Track J

--- scripts/test_script_swissroll.R
+++ scripts/test_script_swissroll.R
@@ -95,7 +95,7 @@ for(i in 1:iterations){
   I <- local_sampler(idx, psu = 10, ssu = 5)
   batch_dict <- dict(I_batch = batch_to_pairs(I))
   session$run(optimizer, feed_dict = batch_dict)
-  print(session$run(model$kern$ARD))
+  print(session$run(model$v_par$mu))
   #print(I)
   if(i %% 10 == 0){
     print(session$run(loss, feed_dict = test_batch))
