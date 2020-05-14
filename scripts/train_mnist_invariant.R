dff1cf32e9abca14f0d8ba71798e04dc212584ef
Martin <marjorg2@gmail.com>
2020-05-14 07:34:59 +0200

    OOM

--- scripts/train_mnist_invariant.R
+++ scripts/train_mnist_invariant.R
@@ -7,12 +7,12 @@ session <- tf$Session()
 session$run(tf$global_variables_initializer())
 
 load("data/mnist/5pca.RDa")
-model$L_scale_matrix <- tf$constant(W,dtype=float_type)
+model$L_scale_matrix <- tf$constant(max(abs(z)) * W,dtype=float_type)
 saver <- tf$train$Saver()
 #' Training
 
 iterations <- 100000
-p <- 50
+p <- 40
 
 # For plotting:
 source("functions/plot_at_iteration.R")
