11bb14e3c1919dd771e5283d4889a79394e17b8a
Martin <marjorg2@gmail.com>
2020-06-02 09:26:48 +0200

    try

--- for_geodesics.R
+++ for_geodesics.R
@@ -55,12 +55,13 @@ K <- length(my_frame[,1])
 my_array <- array(rep(NA, K*4), dim = c(K,2,2))
 
 place_idx <- tf$placeholder(shape = c(1,2),tf$float64)
+meanJtJ <- metric(model,place_idx)
 session$run(tf$global_variables_initializer())
 for(i in 1:K){
   #location <- tf$expand_dims(tf$constant(matrix(my_frame[i,1:2],ncol = 2), dtype = float_type), 0L)
-  my_batch <- dict(place_idx <- matrix(my_frame[i,1:2],ncol = 2))
-  meanJtJ <- session$run(metric(model,place_idx), feed_dict = my_batch)
-  my_array[i,,] <- meanJtJ
+  my_batch <- dict(place_idx = matrix(my_frame[i,1:2],ncol = 2))
+  meanJtJ_out <- session$run(meanJtJ, feed_dict = my_batch)
+  my_array[i,,] <- meanJtJ_out
   print(i)
 }
 
